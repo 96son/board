5e79c26c921cdfe18c2b365b3a1a65766171347c
96son <sonmyeongsu3@gmail.com>
2024-06-23 13:18:45 +0900

    상세조회 메서드 서비스단에서 호출및 구현

--- src/main/java/com/msoffice/work/board/BoardController.java
+++ src/main/java/com/msoffice/work/board/BoardController.java
@@ -1,7 +1,5 @@
 package com.msoffice.work.board;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Map;
 
 import javax.annotation.Resource;
@@ -12,6 +10,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/board")
@@ -20,14 +20,7 @@ public class BoardController {
 	@Resource(name = "boardService")// bean에서 끌어다 쓴 BoardServiceImpl객체
 	private BoardServiceImpl boardServiceImpl;
 	
-	//임시
-	@GetMapping("/newFile")
-	public String newFile() {
-		return "tempfolder/NewFile";
-	}
-	
-	
-	//view
+//view
 	
 	//로그인 페이지
 	@GetMapping("/viewLoginPage")
@@ -41,7 +34,6 @@ public class BoardController {
 		return "pages/board/viewJoinPage"; 
 	}
 	 
-	
 	//메인 페이지
 	@RequestMapping("/viewMainPage")
 	public String viewMainPage() {
@@ -49,11 +41,7 @@ public class BoardController {
 	}
 
 	
-
-	
-	
-	
-	//기능
+//기능
 	
 	// 회원가입 
 	@PostMapping("/regJoin") 
@@ -84,48 +72,43 @@ public class BoardController {
 	
 	
 	
-	
 	// 테스트용 데이터테이블 페이지
 	@RequestMapping("/tables")
-	public String tables(Model model) {
+	public String tables(Model model,Map<String, Object> paramMap) {
 		
-		List<BoardVO> boardList = new ArrayList<>();
-		boardList.add(new BoardVO());
-		boardList.get(0).setBoardTitle("dkfjksdjfklsdjfkljsd");
-		boardList.add(new BoardVO());
-		boardList.get(1).setBoardTitle("11111");
-		boardList.add(new BoardVO());
-		boardList.get(2).setBoardTitle("2222222");
+//		List<BoardVO> boardList = new ArrayList<>();
+//		boardList.add(new BoardVO());
+//		boardList.get(0).setBoardTitle("dkfjksdjfklsdjfkljsd");
+//		boardList.add(new BoardVO());
+//		boardList.get(1).setBoardTitle("11111");
+//		boardList.add(new BoardVO());
+//		boardList.get(2).setBoardTitle("2222222");
 		
-		model.addAttribute("boardList", boardList);
-		
+		model.addAttribute("boardList", boardServiceImpl.selectBoardList(paramMap));
 		
 		return "pages/board/tables";
 	}
 	
 	
-	//게시판 조회(임시)
-	@RequestMapping("/selectBoardList") 
-	public String selectBoardList(Model model, Map<String, Object> paramMap) {
-		//model.addAttribute("boardList", boardServiceImpl.selectBoardList(paramMap));
+	//게시판 상세페이지
+	//		@RequestParam : 스프링은 HTTP 요청 파라미터를 @RequestParam으로 받을 수 있다
+
+	//@ResponseBody //자바 객체를 json 기반의 HTTP Body로 변환
+	@GetMapping("/viewBoardDtlPage")
+	public String viewBoardDtlPage(Model model,@RequestParam Map<String, Object> paramMap) {
+
 		
+		System.out.println("성공함ㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋ");
+		System.out.println(paramMap.get("boardSeq"));
+
+		BoardVO boardVo = boardServiceImpl.selectBoardDtl(paramMap);
+		System.out.println(boardVo.getBoardContent());
+		model.addAttribute("detail",boardVo);
 		
-		return "redirect:/board/tables";
+		return "pages/board/viewBoardDtlPage";
 	}
 	
 	
-	
-	
-	
-	
-	
-	
-	
-	//게시판 페이지
-	@RequestMapping("/viewBoardPage")
-	public String viewBoardPage() {
-		return "pages/board/viewBoardPage";
-	}
 	
 	
 	
